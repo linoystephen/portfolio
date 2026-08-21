b278103affef7fd58ed913839ccdff7f5750cd00
linoystephen <nanobirdtechnologies@gmail.com>
2026-08-21 14:49:45 +0400

    Configure and deploy portfolio Sanity Studio

--- studio/sanity.cli.js
+++ studio/sanity.cli.js
@@ -1,2 +1,11 @@
 import {defineCliConfig} from 'sanity/cli'
-export default defineCliConfig({api:{projectId:process.env.SANITY_STUDIO_PROJECT_ID||'nan7azvw',dataset:process.env.SANITY_STUDIO_DATASET||'production'}})
+
+export default defineCliConfig({
+  api: {
+    projectId: process.env.SANITY_STUDIO_PROJECT_ID || 'nan7azvw',
+    dataset: process.env.SANITY_STUDIO_DATASET || 'production'
+  },
+  deployment: {
+    appId: 'vi9zukhcdwbt2j5k9a4295p4'
+  }
+})
